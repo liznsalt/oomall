abc0e738589e945abc2983dc85a887e870e1e7cb
liznsalt <2274938013@qq.com>
2019-12-04 22:19:10 +0800

    添加商品信息模块

--- goods-info/src/main/java/xmu/oomall/GoodsInfoApplication.java
+++ goods-info/src/main/java/xmu/oomall/GoodsInfoApplication.java
@@ -3,11 +3,13 @@ package xmu.oomall;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 /**
  * @author liznsalt
  */
 @SpringBootApplication
+@EnableRedisRepositories
 @MapperScan("xmu.oomall.mapper")
 public class GoodsInfoApplication {
     public static void main(String[] args) {
