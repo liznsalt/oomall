0040a82b600c83282621c059563f3cf78f724905
liznsalt <2274938013@qq.com>
2019-12-04 13:08:27 +0800

    添加商品信息模块

---
+++ goods-info/src/main/java/xmu/oomall/GoodsInfoApplication.java
@@ -0,0 +1,16 @@
+package xmu.oomall;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liznsalt
+ */
+@SpringBootApplication
+@MapperScan("xmu.oomall.mapper")
+public class GoodsInfoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GoodsInfoApplication.class, args);
+    }
+}
