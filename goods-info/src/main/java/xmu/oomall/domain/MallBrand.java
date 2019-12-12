3290c574fb9fccbf393ef66cbec11c6c49d6b1d1
zhang you kun <1684864253@qq.com>
2019-12-13 00:06:32 +0800

    有坤在提交了

--- goods-info/src/main/java/xmu/oomall/domain/MallBrand.java
+++ goods-info/src/main/java/xmu/oomall/domain/MallBrand.java
@@ -8,5 +8,15 @@ import standard.oomall.domain.Brand;
  */
 @Alias("mallBrand")
 public class MallBrand extends Brand {
-
+    public MallBrandPo setToMallBrandPo(){
+         MallBrandPo brandPo = new MallBrandPo();
+         brandPo.setId(this.getId());
+         brandPo.setName(this.getName());
+         brandPo.setDescription(this.getDescription());
+         brandPo.setPicUrl(this.getPicUrl());
+         brandPo.setGmtCreate(this.getGmtCreate());
+         brandPo.setGmtModified(this.getGmtModified());
+         brandPo.setBeDeleted(this.getBeDeleted());
+         return brandPo;
+    }
 }
--- goods-info/src/main/java/xmu/oomall/domain/MallBrandPo.java
+++ goods-info/src/main/java/xmu/oomall/domain/MallBrandPo.java
@@ -5,4 +5,15 @@ import standard.oomall.domain.BrandPo;
 
 @Alias("mallBrandPo")
 public class MallBrandPo extends BrandPo {
+    public MallBrand setToMallBrand(){
+        MallBrand brand = new MallBrand();
+        brand.setId(this.getId());
+        brand.setName(this.getName());
+        brand.setDescription(this.getDescription());
+        brand.setPicUrl(this.getPicUrl());
+        brand.setGmtCreate(this.getGmtCreate());
+        brand.setGmtModified(this.getGmtModified());
+        brand.setBeDeleted(this.getBeDeleted());
+        return brand;
+    }
 }
